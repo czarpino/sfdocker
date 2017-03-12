f3d76afa7d549c481b001c3b991d7609b21f16b9
Czar Pino <inbox@czarpino.com>
2017-03-12 16:04:11 +0800

    Added back starting apache

--- php/bootstrap.sh
+++ php/bootstrap.sh
@@ -2,3 +2,6 @@
 
 # See bootstrap.sh.sample for ideas on how to utilize this bootstrap.
 # To speed up initialization, commands are ommitted
+
+echo "Starting server..."
+/usr/local/bin/apache2-foreground
